0ec502d265a66985f7b1e1b02ac27669254f96ad
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-07-22 09:52:40 -0300

    Adicionar teste "Adicionar"
    Adicionado o arquivo de testes para o método "Adicionar" da classe "OfertaViagemDAL".

---
+++ JornadaMilhas.Test.Integracao/OfertaViagemDAL/Adicionar.cs
@@ -0,0 +1,28 @@
+﻿using JornadaMilhas.Dados;
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test.Integracao.OfertaViagemDal;
+
+public class Adicionar
+{
+    [Fact]
+    public void RegistersOfferInDatabaseIfOfferIsValid()
+    {
+        // Arrange
+        var route = new Rota("Irrelevant", "Irrelevant");
+        var period = new Periodo(DateTime.Now, DateTime.Now);
+        double price = 350.0;
+
+        var offer = new OfertaViagem(route, period, price);
+        var dal = new OfertaViagemDAL();
+
+        // Act
+        dal.Adicionar(offer);
+
+        // Assert
+        var includedOffer = dal.RecuperarPorId(offer.Id);
+
+        Assert.NotNull(includedOffer);
+        Assert.Same(offer, includedOffer);
+    }
+}
