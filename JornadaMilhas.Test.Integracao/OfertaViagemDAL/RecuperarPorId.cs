e0cdc04bd9bddada8e01d295dc7310e15004a864
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-07-23 10:40:02 -0300

    Adicionar "RecuperarPorId"
    Adicionado a classe de testes "RecuperarPorId", já utilizando a nova Collection adicionada anteriormente.

---
+++ JornadaMilhas.Test.Integracao/OfertaViagemDAL/RecuperarPorId.cs
@@ -0,0 +1,41 @@
+﻿using JornadaMilhas.Dados;
+using JornadaMilhas.Test.Integracao.Fixtures;
+using Microsoft.EntityFrameworkCore.Storage;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao.OfertaViagemDAL;
+
+[Collection(nameof(ContextCollection))]
+public class RecuperarPorId : IDisposable
+{
+    private readonly JornadaMilhasContext _context;
+    private readonly IDbContextTransaction _transaction;
+
+    public RecuperarPorId(ITestOutputHelper output, ContextFixture fixture)
+    {
+        _context = fixture.Context;
+
+        output.WriteLine(_context.GetHashCode().ToString());
+
+        _transaction = _context.Database.BeginTransaction();
+    }
+
+    [Fact]
+    public void ReturnsNullIfIdDoesNotExist()
+    {
+        // Arrange
+        var dal = new Dados.OfertaViagemDAL(_context);
+
+        // Act
+        var recoveredOffer = dal.RecuperarPorId(-2);
+
+        // Assert
+        Assert.Null(recoveredOffer);
+    }
+
+    public void Dispose()
+    {
+        _transaction.Rollback();
+        _transaction.Dispose();
+    }
+}
