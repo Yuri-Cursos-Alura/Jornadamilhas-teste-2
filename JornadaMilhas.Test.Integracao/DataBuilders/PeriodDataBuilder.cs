52154dd4e58c97f6349d734754efe918e9412eeb
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-07-24 11:26:48 -0300

    Adicionar "PeriodDataBuilder"
    Adicionado a pasta "DataBuilders", com a classe "PeriodDataBuilder" com o trabalho de gerar automáticamente objectos do tipo "Periodo" com dados fakes.

---
+++ JornadaMilhas.Test.Integracao/DataBuilders/PeriodDataBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test.Integracao.Fakers;
+
+public class PeriodDataBuilder : Faker<Periodo>
+{
+    public DateTime? InitialDate { get; set; }
+    public DateTime? FinalDate { get; set; }
+
+    public PeriodDataBuilder()
+    {
+        CustomInstantiator(f =>
+            {
+                DateTime dataInicio = InitialDate ?? f.Date.Soon();
+                DateTime dataFim = FinalDate ?? dataInicio.AddDays(30);
+                return new Periodo(dataInicio, dataFim);
+            });
+    }
+
+    public Periodo Build() => Generate();
+}
