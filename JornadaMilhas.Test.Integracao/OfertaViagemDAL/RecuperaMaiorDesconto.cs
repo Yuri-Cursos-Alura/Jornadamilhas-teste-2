0f9ced3c5554fe106dcdad494f6864a7fec4a235
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-07-24 10:26:41 -0300

    Adicionar "RecuperaMaiorDesconto"
    Adicionado a classe de testes para o método "RecuperaMaiorDesconto" da classe "OfertaViagemDAL".

---
+++ JornadaMilhas.Test.Integracao/OfertaViagemDAL/RecuperaMaiorDesconto.cs
@@ -0,0 +1,27 @@
+﻿using JornadaMilhas.Dados;
+using JornadaMilhas.Test.Integracao.Fixtures;
+using Microsoft.EntityFrameworkCore.Storage;
+using Xunit.Abstractions;
+
+namespace JornadaMilhas.Test.Integracao.OfertaViagemDAL;
+
+public class RecuperaMaiorDesconto : IDisposable
+{
+    private readonly JornadaMilhasContext _context;
+    private readonly IDbContextTransaction _transaction;
+
+    public RecuperaMaiorDesconto(ITestOutputHelper output, ContextFixture fixture)
+    {
+        _context = fixture.Context;
+
+        _transaction = _context.Database.BeginTransaction();
+    }
+
+
+
+    public void Dispose()
+    {
+        _transaction.Rollback();
+        _transaction.Dispose();
+    }
+}
